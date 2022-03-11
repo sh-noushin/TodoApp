ff0489bd810b0953941796ab1aa67cfba1f906c9
nshokrollahi951 <nshokrollahi951@gmail.com>
2022-03-11 18:31:30 +0330

    Added ITodoUserService and Dtos

---
+++ TodoApp.Application.Contract/Users/Dtos/Responses/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Application.Contract.Users.Dtos.Responses
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TodoApp.Application.Contract/Users/ITodoUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Application.Contract.Core.Dtos.Responses;
+using TodoApp.Application.Contract.Users.Dtos.Requests;
+using TodoApp.Application.Contract.Users.Dtos.Responses;
+
+namespace TodoApp.Application.Contract.Users
+{
+    public interface ITodoUserService
+    {
+        Task CreateAsync(UserCreateRequest input);
+        Task UpdateAsync(Guid id, UserUpdateRequest input);
+        Task DeleteAsync(Guid id);
+        Task<PagedResultResponse<UserResponse>> GettAllAsync(GetUsersRequest filter);
+        Task<UserResponse> GetByIdAsync(Guid id);
+    }
+}
