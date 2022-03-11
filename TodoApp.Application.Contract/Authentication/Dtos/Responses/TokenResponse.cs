870c3ea6fc1ced8887d831d0c817903304e47cf3
nshokrollahi951 <nshokrollahi951@gmail.com>
2022-03-11 19:01:26 +0330

    Added IAuthService

---
+++ TodoApp.Application.Contract/Authentication/Dtos/Responses/TokenResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Application.Contract.Authentication.Dtos.Responses
+{
+    public class TokenResponse
+    {
+        public Guid Value { get; set; }
+    }
+}
---
+++ TodoApp.Application.Contract/Authentication/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Application.Contract.Authentication.Dtos.Requests;
+using TodoApp.Application.Contract.Authentication.Dtos.Responses;
+
+namespace TodoApp.Application.Contract.Authentication
+{
+    public interface IAuthService
+    {
+        Task<TokenResponse> LoginAsync(LoginRequest input);
+    }
+}
